2b8881e787a020aca4f67367d3fe59b2ed56dc5f
Mohamed Elwakeel <mo7mdhassan7@gmail.com>
2023-07-31 09:47:41 +0300

    deploy commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,9 @@ import "./index.css";
 import App from "./App";
 import { GlobalProvider } from "./content/context";
 import { BrowserRouter } from "react-router-dom";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if (ProcessingInstruction.env.NODE_ENV === "production") disableReactDevTools();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
